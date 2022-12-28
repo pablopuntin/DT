10f2d0a8fd1f8118208ef6dc1692c29ea0645686
pablopuntin <pablopuntin@gmail.com>
2022-12-28 20:17:17 -0300

    primer commit

---
+++ JS/global.js
@@ -0,0 +1,32 @@
+//titulo bicicleteria
+let writing = str => {
+    let arrFromStr =str.split('');
+    let i=0;
+    let printStr =setInterval(function(){
+        document.getElementById("logo1").innerHTML += arrFromStr[i];
+        i++;
+       
+       if (i === arrFromStr.length){
+           clearInterval(printStr);
+       }
+    },280);
+};
+
+setInterval(writing('MOTOREP & BICICLETERIA "DT"'), 100);
+
+//subtitulo GUSTAVO LEAL
+
+let writing1 = str1 => {
+    let arrFromStr1 =str1.split('');
+    let i=0;
+    let printStr1 =setInterval(function(){
+        document.getElementById("logo2").innerHTML += arrFromStr1[i];
+        i++;
+       
+       if (i === arrFromStr1.length){
+           clearInterval(printStr1);
+       }
+    },280);
+};
+
+setInterval(writing1('DE GUSTAVO LEAL'), 100);
